1d305acbe535165dc51d1fc7eb088a7e6bd1b7e9
Eber Leguizamon <eberlegza@gmail.com>
2022-09-14 16:02:52 -0300

    refactor: add logic to sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -26,15 +26,37 @@ exports.sendEmail = async (subject, content) => {
         accessToken: accesToken
       }
     })
-
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transport.verify(function (error, success) {
+        if (error) {
+          console.log(error)
+          reject(error)
+        } else {
+          console.log('Server is ready to take our messages')
+          resolve(success)
+        }
+      })
+    })
     const mailOptions = {
       from: 'Portfolio',
       to: process.env.USER_EMAIL,
       subject,
       html: content
-
     }
-    return await transport.sendMail(mailOptions)
+
+    await new Promise((resolve, reject) => {
+      // send mail
+      transport.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err)
+          reject(err)
+        } else {
+          console.log(info)
+          resolve(info)
+        }
+      })
+    })
   } catch (error) {
     console.log(error)
   }
