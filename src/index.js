4d6f18f2a6ef6d0cf95ccbc357dbd4bdcf0347d4
Eber Leguizamon <eberlegza@gmail.com>
2022-09-14 14:07:40 -0300

    refactor: add some comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
+// Packages
 const express = require('express')
-const app = express()
-const routes = require('./routes')
+const cors = require('cors')
 const dotenv = require('dotenv')
+
+// Routes
+const routes = require('./routes')
 
 dotenv.config()
 
+const app = express()
+
+// Set Body parser, reading data from body into req.body
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Implement CORS
+app.use(cors())
+
+// API Routes
 app.use('/api', routes)
 
+// Server
 const serverPort = process.env.PORT
 
 app.listen(serverPort, () => console.log(`
